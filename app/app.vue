d741d7588d27ec8ea0d0ff6e822b2c0b2653a0ab
Danil.Galyamov <Danil.Galyamov@tatar.ru>
2025-07-17 16:11:28 +0300

    make start menu

---
+++ app/app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative">
+    <h1 class="text-fuchsia-200 text-center text-3xl font-heading mb-8
+                      animate-fade-in-down animate-duration-30000">Who Is It?</h1>
+
+    <div class="flex gap-4">
+      <button class="bg-purple-500
+                hover:bg-purple-600
+                text-white
+                font-medium
+                py-3 px-8
+                rounded-lg
+                transition-all
+                duration-300
+                hover:scale-105">
+        Начать игру</button>
+
+      <button class="border-gray-600
+              text-white
+              py-3 px-8
+              rounded-lg
+              transition-all
+              duration-300
+              hover:scale-105
+            hover:bg-gray-500">
+        О игре
+      </button>
+
+    </div>
+    <div class="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-transparent via-purple-500 to-transparent mt-2"></div>
+    <div class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-transparent via-pink-500 to-transparent mb-2"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+</style>
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config> {
+    content: [
+        './components/**/*.{js,vue,ts}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
+        './app.vue',
+        './error.vue'
+    ],
+    theme: {
+        extend: {
+            fontFamily:{
+                sans: ['Inter', 'sans-serif'],
+                heading: ['Montserrat', 'sans-serif']
+            },
+            animation:{
+                'fade-in-down': 'fadeInDown 2s ease-out'
+            },
+            keyframes:{
+                fadeInDown:{
+                    '0%': {opacity: '0', transForm: 'translateY(-20px)'},
+                    '50%': {opacity: '0', transForm: 'translateY(-10px)'},
+                    '100%': {opacity: '1', transForm: 'translateY(0)'}
+                }
+            }
+        },
+    },
+
+    plugins: [],
+}
